6ff3c7bfaace21abab12a75f9e4f79eee978fdf1
ClementVallois <clement.vallois@oclock.school>
2024-03-27 17:12:49 +0100

    Add a Search and Auth resources

--- backend-api/src/app.module.ts
+++ backend-api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { SearchModule } from './search/search.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, SearchModule],
   controllers: [AppController],
   providers: [AppService],
 })
