d4cb4aa933ebf5f602bc65f60c53e65310cf7ef7
ClementVallois <clement.vallois@oclock.school>
2024-03-28 01:50:03 +0100

    Create generic clean architecture file structure

--- backend-api/src/app.module.ts
+++ backend-api/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { SearchModule } from './search/search.module';
+import { AuthModule } from './infrastructure/auth/auth.module';
+import { SearchModule } from './application/search/search.module';
+import { UserModule } from './application/user/user.module';
+import { PostModule } from './application/post/post.module';
+import { PortfolioModule } from './application/portfolio/portfolio.module';
 
 @Module({
-  imports: [AuthModule, SearchModule],
+  imports: [AuthModule, SearchModule, UserModule, PostModule, PortfolioModule],
   controllers: [AppController],
   providers: [AppService],
 })
