616f9fb5881a3b3467892e716a874f170a9e8f22
dessna12 <92819503+dessna12@users.noreply.github.com>
2024-03-28 09:18:18 +0100

    give the structure of the files and the dashboard

---
+++ frontend-backoffice/src/components/layout/SideBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- Side bar -->
+    <div class="w-[400px] h-full bg-gray-200 text-white">
+        <div class="h-[50px] bg-gray-900 flex justify-start items-center">
+            <div class="px-[20px] h-[64px] flex items-center">
+                <img src="/logoArtFolio.png" alt="Favicon" class="h-4/5 object-cover" >
+                <h1 class="font-bold text-xl">Admin</h1>
+            </div>
+        </div>
+        <div class="h-[calc(100vh-50px)] bg-gray-800 py-[20px]">
+            <div class="px-[20px] flex-col space-y-[10px]">
+                <router-link to="/" class="inline-flex relative items-center py-[10px] w-full text-sm font-medium border-gray-200 hover:bg-gray-200 group hover:text-gray-800">
+                    <i class="fas fa-home text-white text-l px-[10px] group-hover:text-gray-800"></i>
+                    <p>Home</p>
+                </router-link>
+                <router-link to="/inventory" class="inline-flex relative items-center py-[10px] w-full text-sm font-medium border-gray-200 hover:bg-gray-300 group hover:text-gray-800">
+                    <i class="fas fa-boxes-stacked text-white text-l px-[10px] group-hover:text-gray-800"></i>
+                    <p>Inventory</p>
+                </router-link>                
+            </div>
+        </div>    
+    </div><!-- Side bar -->
+</template>
+
+<script setup>
+let showSideBar = true
+
+const toggleSideBar = () => {
+    showSideBar=!showSideBar
+}
+
+</script>
---
+++ frontend-backoffice/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="h-[calc(100%-50px)] bg-white flex items-center justify-center">
+        This is the dashboard Admin
+    </div>
+</template>
+
+<script setup>
+let showSideBar = true
+
+const toggleSideBar = () => {
+    showSideBar=!showSideBar
+}
+
+</script>
