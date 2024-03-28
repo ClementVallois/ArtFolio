3b49cf7b4c3e7c69199eefb532350ef7253eb26e
eugenie desaunai <eugeniedesaunai@gmail.com>
2024-03-28 16:34:56 +0100

    add footer

--- frontend-main/src/App.vue
+++ frontend-main/src/App.vue
@@ -3,10 +3,11 @@
     <div class="w-screen h-screen flex">
 
     </div>
+    <FooterComponent />
 </template>
 
 <script setup>
 import NavComponent from '@/components/layout/NavComponent.vue';
-
+import FooterComponent from '@/components/layout/FooterComponent.vue';
 
 </script>
