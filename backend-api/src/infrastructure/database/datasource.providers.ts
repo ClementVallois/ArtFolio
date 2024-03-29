b0caa3c47ade9a8c83685baa807c53949fa0ec1f
ClementVallois <clement.vallois@oclock.school>
2024-03-29 11:24:47 +0100

    Refactor the datasource module

---
+++ backend-api/src/infrastructure/database/datasource.providers.ts
@@ -0,0 +1,27 @@
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { User } from 'src/domain/user/user.entity';
+import { Post } from 'src/domain/post/post.entity';
+import { Asset } from 'src/domain/asset/asset.entity';
+
+@Injectable()
+export class DatasourceProvider implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('DB_API_HOST'),
+      port: this.configService.get<number>('DB_API_PORT'),
+      username: this.configService.get<string>('DB_API_USER'),
+      password: this.configService.get<string>('DB_API_PASSWORD'),
+      database: this.configService.get<string>('DB_API_NAME'),
+      entities: [User, Post, Asset],
+      synchronize: true,
+      logging: true,
+      migrationsRun: true,
+      migrations: [__dirname + '/../**/migrations/*{.ts,.js}'],
+    };
+  }
+}
