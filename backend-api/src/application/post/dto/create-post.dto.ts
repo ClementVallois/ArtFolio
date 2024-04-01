10770fb1abf6061ab92158d84b2def6d9eed4496
ClementVallois <clement.vallois@oclock.school>
2024-04-01 17:35:28 +0200

    Reworked the post DTOs to add UUID check and inheritance

--- backend-api/src/application/post/dto/create-post.dto.ts
+++ backend-api/src/application/post/dto/create-post.dto.ts
@@ -1,1 +1,10 @@
-export class CreatePostDto {}
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePostDto {
+  @IsBoolean({ message: 'Must be a boolean' })
+  @IsNotEmpty({ message: 'Must not be empty' })
+  isPinned: boolean;
+
+  @IsString({ message: 'Must be a string' })
+  description: string;
+}
--- backend-api/src/application/post/dto/update-post.dto.ts
+++ backend-api/src/application/post/dto/update-post.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
+import { IsNotEmpty, IsUUID } from 'class-validator';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends PartialType(CreatePostDto) {
+  @IsNotEmpty({ message: 'Must not be empty' })
+  @IsUUID('4', { message: 'Must be a valid uuid' })
+  id: string;
+}
