75aaf01b2086c4f2d7ecff0a3bceaa77d17ab7de
ClementVallois <clement.vallois@oclock.school>
2024-04-03 00:03:40 +0200

    Remove a wrong check in post update DTO

--- backend-api/src/application/post/dto/update-post.dto.ts
+++ backend-api/src/application/post/dto/update-post.dto.ts
@@ -1,9 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
-import { IsNotEmpty, IsUUID } from 'class-validator';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {
-  @IsNotEmpty({ message: 'ID must not be empty' })
-  @IsUUID('4', { message: 'ID must be a valid UUID' })
-  id: string;
-}
+export class UpdatePostDto extends PartialType(CreatePostDto) {}
