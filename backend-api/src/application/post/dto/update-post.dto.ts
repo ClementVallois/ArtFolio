7a9f15f17ef224f27c3911bf0145e583decc962c
ClementVallois <clement.vallois@oclock.school>
2024-04-02 23:49:33 +0200

    Update DTOs wording

--- backend-api/src/application/post/dto/update-post.dto.ts
+++ backend-api/src/application/post/dto/update-post.dto.ts
@@ -3,7 +3,7 @@ import { CreatePostDto } from './create-post.dto';
 import { IsNotEmpty, IsUUID } from 'class-validator';
 
 export class UpdatePostDto extends PartialType(CreatePostDto) {
-  @IsNotEmpty({ message: 'Must not be empty' })
-  @IsUUID('4', { message: 'Must be a valid uuid' })
+  @IsNotEmpty({ message: 'ID must not be empty' })
+  @IsUUID('4', { message: 'ID must be a valid UUID' })
   id: string;
 }
