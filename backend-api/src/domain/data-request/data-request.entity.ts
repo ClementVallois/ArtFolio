a05f0c666362c54e6cb2392d1dc8e9a3c0b8f131
ClementVallois <clement.vallois@oclock.school>
2024-04-03 22:06:58 +0200

    Create a DataRequest Entity

--- backend-api/src/domain/data-request/data-request.entity.ts
+++ backend-api/src/domain/data-request/data-request.entity.ts
@@ -1,1 +1,27 @@
-export class DataRequest {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+import { User } from '../user/user.entity';
+
+@Entity('data_request')
+export class DataRequest {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  user: User;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ type: 'timestamp' })
+  deletedAt: Date;
+}
