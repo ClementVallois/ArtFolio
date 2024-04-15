05ce3179f297fbec4f28d583b2335b5b029cafd8
dessna12 <92819503+dessna12@users.noreply.github.com>
2024-04-15 10:24:13 +0200

    Revert "Merge pull request #31 from Apothose-Oclock-ArtFolio/ND_feat_frontend-backoffice_setup-project"
    This reverts commit 772ed96b8b915aadf2e1468d728ef609097f65b7, reversing
    changes made to 7c3e61f80a1daa50b88fcf73a28eb6b8f32fd1da.

--- frontend-backoffice/src/App.vue
+++ frontend-backoffice/src/App.vue
@@ -1,22 +1,47 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
 <template>
-  <div class="w-screen h-screen flex"> 
-    <SideBar v-show=showSideBar />
-    <div class="w-full h-full bg-gray-400">
-      <TopBar @toggleSideBar="toggleSideBar()"/>
-      <router-view />
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
     </div>
-  </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
 </template>
 
-<script setup>
-import { ref } from 'vue'
-import SideBar from '@/components/layout/SideBar.vue';
-import TopBar from '@/components/layout/TopBar.vue';
-
-const showSideBar = ref(true)
-
-const toggleSideBar = () => { 
-  showSideBar.value=!showSideBar.value
+<style scoped>
+header {
+  line-height: 1.5;
 }
 
-</script>
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
