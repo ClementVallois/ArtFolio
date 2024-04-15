391575331630416037d01732c3043d0e3c9a8a18
ClementVallois <clement.vallois@oclock.school>
2024-04-16 01:22:37 +0200

    Move the jwt folder

--- backend-api/src/infrastructure/services/jwt/jwt.module.ts
+++ backend-api/src/infrastructure/services/jwt/jwt.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from '../../common/stategies/jwt.strategy';
+import { JwtStrategy } from './stategies/jwt.strategy';
 import { UserModule } from 'src/presentation/user/user.module';
 
 @Module({
