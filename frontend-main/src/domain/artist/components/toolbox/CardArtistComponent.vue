0aba6418cb8dcfa094e431e119d92dd956b2af79
ClementVallois <clement.vallois@oclock.school>
2024-08-07 11:51:36 +0200

    add little design fixes in main frontend

--- frontend-main/src/domain/artist/components/toolbox/CardArtistComponent.vue
+++ frontend-main/src/domain/artist/components/toolbox/CardArtistComponent.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="flex flex-col m-auto max-w-[95%] mb-[1rem] rounded overflow-hidden shadow-lg lg:w-[30vw]">
         <router-link :to="{ name: 'artist', params: { artistId: props.artistId } }" class="flex">
-            <div class="max-w-[10%]">  
+            <div class="max-w-[10%] py-2">
                 <img src="@/assets/img/profil.png" alt="" class="relative rounded-lg overflow-hidden">
-            </div>   
+            </div>
             <div class="text-start pl-[1rem]">
-                <p class="text-xs font-boldText lg:text-base">{{ props.artistFirstName + " " + props.artistLastName }}</p>
+                <p class="text-xs font-boldText lg:text-base">{{ props.artistFirstName + " " + props.artistLastName }}
+                </p>
                 <p class="text-[0.7rem] font-lightText">{{ props.artistUsername }}</p>
-            </div>         
+            </div>
         </router-link>
         <img class="max-h-[15rem] self-center  lg:max-h-[20rem]" src="@/assets/img/peinture.png">
         <div class="px-6 py-4 overflow-y-auto" style="height: calc(30vh - 7rem);">
@@ -37,4 +38,3 @@ const props = defineProps({
 
 
 </script>
-
