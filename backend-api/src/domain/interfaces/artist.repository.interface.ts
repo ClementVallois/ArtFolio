f6b7733a5d84f2910d5c7e4f18f962cd0254095b
ClementVallois <github.tricky471@passmail.com>
2026-04-29 10:50:02 +0200

    fix: use CreateArtistDto in IArtistRepository instead of any

--- backend-api/src/domain/interfaces/artist.repository.interface.ts
+++ backend-api/src/domain/interfaces/artist.repository.interface.ts
@@ -1,9 +1,10 @@
 import { UpdateArtistDto } from 'src/presentation/dto/artist/update-artist.dto';
 import { User as Artist } from '../entities/user.entity';
 import { ArtistId } from '../value-objects/artistId';
+import { CreateArtistDto } from 'src/presentation/dto/artist/create-artist.dto';
 
 export interface IArtistRepository {
-  createArtist(artist: any): Promise<Artist>;
+  createArtist(artist: CreateArtistDto): Promise<Artist>;
   findArtistById(id: ArtistId): Promise<Artist>;
   findAllArtists(): Promise<Artist[]>;
   findAllByDescCreateDate(): Promise<Artist[]>;
